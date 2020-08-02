408afbf36c39eb1c32b1c0e56e6241201ec9de6a
ldg <7sus7@naver.com>
2020-08-02 16:30:09 +0900

    Category Select With News Api Request Success

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState, useCallback } from 'react';
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback((category) => setCategory(category), []);
   return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea rows={7} value={JSON.stringify(data, null, 2)} readonly />
-      )}
-    </div>
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+    </>
   );
 };
 
